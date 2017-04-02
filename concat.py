9223d7570fc577ab49240874891ad381f67c09cd
Sanchet Boggram <sanchet@gatech.edu>
2017-04-01 20:40:08 -0400

    added script to combine stats.csv and dns_record_*.csvand store the results in sample.csv. Contents of each dns record file is seperated by ###

---
+++ concat.py
@@ -0,0 +1,36 @@
+import csv
+import os
+from os import listdir,system
+from os.path import isfile, join
+import sys
+
+#getting list of dns record files
+mypath = os.getcwd()
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+dns_files = []
+for file in files:
+    if "dns_record_" in file:
+        dns_files.append(file)
+dns_files = sorted(dns_files)
+
+#open stats.csv
+stat_file = open("stats.csv")
+stats_reader = csv.reader(stat_file)
+
+#create output file
+out_file = open("sample.csv", "w")
+stats_out = stats_reader.next()
+
+#writing stats data to output file
+out_writer = csv.writer(out_file)
+out_writer.writerow(stats_out)
+
+#writing dns data into output file
+for file in dns_files:
+    dns_file = open(file)
+    dns_reader = csv.reader(dns_file)
+    ignore = dns_reader.next()
+    for row in dns_reader:
+        out_writer.writerow(row)
+    out_writer.writerow(['###'])
+out_file.close()
