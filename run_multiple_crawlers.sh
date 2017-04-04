2ac2703e4ada7d29498eb32b1aa90cf7e6695250
Karthik Balakrishnan <karthik.b@gatech.edu>
2017-04-04 12:00:16 -0400

    Updated multiple crawlers' run and stop script

--- run_multiple_crawlers.sh
+++ run_multiple_crawlers.sh
@@ -8,5 +8,5 @@ fi
 DATE=`date +"%m-%d-%y"`
 mkdir -p logs/$DATE
 for i in `seq 1 $1`; do
-    bash run_crawler.sh crawl_lists/website_list_$i > logs/$DATE/log_$i 2>logs/$DATE/log_error_$i &
+    bash run_crawler.sh crawl_lists/website_list_$i >> logs/$DATE/log_$i 2>> logs/$DATE/log_error_$i &
 done
--- stop_multiple_crawlers.sh
+++ stop_multiple_crawlers.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# Kill all the crawlers (run_crawler.sh and any straggling crawler.js processes)
-ps aux | grep -e "run_crawler.sh" -e "crawler.js" | awk '{print $2}' > crawler_pids
+# Kill all the crawlers (run_crawler.sh and any straggling start.sh, crawler.js processes)
+ps aux | grep -e "run_crawler.sh" -e "crawler.js" -e "start.sh" -e "get_dns.py" -e "inotifywait" | awk '{print $2}' > straggler_pids
 while read -r PID; do
     kill -9 $PID
-done < crawler_pids
-
-rm crawler_pids
-
-# Kill all inotify events
-ps aux | grep inotifywait | awk '{print $2}' > pid
-while read -r PID; do
-    kill -9 $PID
-done < pid
-
-rm pid
+done < straggler_pids
+rm straggler_pids
 
 # Echo the completed websites
 DATE=`date +"%m-%d-%y"`
